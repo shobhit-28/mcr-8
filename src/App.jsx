d104c41ab8c6f3dfe272862473038e16dcce0d83
shobhit-28 <shobhitraj34@gmail.com>
2023-07-13 18:52:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import { HomePage } from "./Pages/HomePage/HomePage"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,5 @@
+export const HomePage = () => {
+  return (
+    <h1 className="text-5xl text-red-900">Home</h1>
+  )
+}
